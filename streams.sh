724515820baf1706913c96a1641e1ff1c837c7a4
Chris Travis <christravis@users.noreply.github.com>
2014-01-18 15:47:46 +0000

    Updating streams login script to set up IBM Java environment

--- streams.sh
+++ streams.sh
@@ -1,6 +1,11 @@
 if [[ $UID -gt 499 && -e /etc/alternatives/infosphere-streams-3/bin/streamsprofile.sh ]]; then
 	source /etc/alternatives/infosphere-streams-3/bin/streamsprofile.sh
 
+	if [ -e /etc/alternatives/infosphere-streams-3/java/bin/java ]; then
+		export JAVA_HOME=/etc/alternatives/infosphere-streams-3/java
+		export PATH=$JAVA_HOME/bin:$PATH
+	fi
+
 	if [ ! -e $HOME/.streams/key/$USER.pem ]; then
 		echo "You don't seem to have a Streams key! :("
 		echo "I'm going to try and create one for you!"
